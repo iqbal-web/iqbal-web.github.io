302aef2b8dbae872b1ddb02cec860ef11125ac88
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2025-11-14 23:30:14 +0600

    Remove Newsletter component from Home page
    Commented out the import and usage of the Newsletter component in the Home page. This change temporarily disables the Newsletter section from rendering.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
-import Newsletter from "./components/Newsletter";
+// import Newsletter from "./components/Newsletter";
 import Projects from "./components/Projects";
 
 
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <Blogs />
-      <Newsletter />
+      {/* <Newsletter /> */}
     </main>
   );
 } 
