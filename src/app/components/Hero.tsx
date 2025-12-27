3eede29249b1bd1635ab956f27adb1242edde075
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2025-12-27 21:07:10 +0600

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
             {...fadeInUp}
             transition={{ delay: 0.4 }}
           >
-            Front End Engineer, and efficient WordPress Engineer
+            Front End Engineer, and WordPress Developer
           </motion.p>
           <motion.div 
             className="flex justify-center space-x-4 mb-8"
